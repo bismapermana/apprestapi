5091360cb8841c53953166aa5cd93d9a23d7e644
bismapermana <bismapermana@gmail.com>
2021-10-02 15:53:29 +0700

    post data

--- controller.js
+++ controller.js
@@ -31,3 +31,21 @@ exports.tampilsemuamahasiswaid = function (req, res) {
     }
   );
 };
+
+exports.tambahmahasiswa = function (req, res) {
+  var nim = req.body.nim;
+  var nama = req.body.nama;
+  var jurusan = req.body.jurusan;
+
+  connection.query(
+    "INSERT INTO mahasiswa (nim, nama, jurusan) VALUES(?,?,?)",
+    [nim, nama, jurusan],
+    function (error, rows, fields) {
+      if (error) {
+        console.log(error);
+      } else {
+        response.ok("berhasil menambah data", res);
+      }
+    }
+  );
+};
